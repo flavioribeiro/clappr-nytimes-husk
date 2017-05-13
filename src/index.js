ef0e876a8f699922aa3aabb6803178f38f80701e
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-05-13 15:07:15 -0400

    index: load spinner as default

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import Clappr from 'clappr'
+import NYTSpinner from './nyt_spinner'
 
 let player = new Clappr.Player({
   parent: '.player',
-  autoPlay: !Clappr.Browser.isMobile,
   disableKeyboardShortcuts: true,
-  disableVideoTagContextMenu: true,
-  autoSeekFromUrl: false,
   width: '640px',
   height: '360px',
   source: "https://vp.nyt.com/video/2017/05/09/72421_1_11bearsears-vid_wg_360p.mp4",
   playInLine: true,
+  plugins: [NYTSpinner]
 })
