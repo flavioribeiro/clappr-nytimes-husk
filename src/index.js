26cea727971350fa2b600bfc0692bd0ec9c849f1
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-05-13 21:26:24 -0400

    make the play button work

--- src/index.js
+++ src/index.js
@@ -10,5 +10,6 @@ let player = new Clappr.Player({
   poster: "https://static01.nyt.com/images/2014/05/14/multimedia/clark-rhubarb-shake/clark-rhubarb-shake-videoSixteenByNine1050-v2.jpg",
   source: "https://vp.nyt.com/video/2017/05/09/72421_1_11bearsears-vid_wg_360p.mp4",
   playInLine: true,
+  chromeless: true,
   plugins: {container:[NYTSpinner], core: [NYTControls]}
 })
--- src/nyt_controls/index.js
+++ src/nyt_controls/index.js
@@ -4,19 +4,58 @@ import controlsStyle from './public/controls.scss'
 
 export default class NYTControls extends UICorePlugin {
   get name() { return 'NYTControls' }
-  get template() { return template(controlsHTML) }
-  shouldRender() { return true }
 
-  constructor(core) {
-    super(core.options)
-    this.core = core
-    this.renderControls()
+  get events() {
+    return {
+      'click .controls-play': 'togglePlay'
+    }
   }
 
-  renderControls() {
-    const style = Styler.getStyleFor(controlsStyle)
-    this.core.$el.append(controlsHTML)
-    this.core.$el.append(style[0])
+  togglePlay() { 
+    if (this.core.mediaControl.container.isPlaying()) {
+      this.core.mediaControl.pause()
+    } else {
+      this.core.mediaControl.play()
+    }
+  }
+
+  bind() {
+		this.ui = {
+			play: this.$el.find('.controls-play'),
+			progress: this.$el.find('.controls-progress'),
+			progressTimeline: this.$el.find('.controls-progress-timeline'),
+			progressBuffer: this.$el.find('.controls-progress-buffer'),
+			progressTime: this.$el.find('.controls-progress-time'),
+			marker: this.$el.find('.controls-progress-marker'),
+			duration: this.$el.find('.controls-duration'),
+			currentTime: this.$el.find('.controls-current-time'),
+		};
+
+    let container = this.core.mediaControl.container
+    this.listenTo(container, Events.CONTAINER_PLAY, this.onPlay)
+		this.listenTo(container, Events.CONTAINER_PAUSE, this.onPause)
+    this.listenTo(container, Events.CONTAINER_TIMEUPDATE, this.onTimeUpdate)
+		this.listenTo(container, Events.CONTAINER_SEEK, this.onSeek)
+		this.listenTo(container, Events.CONTAINER_ERROR, this.onError)
+  }
+
+  onPlay() {
+    this.ui.play.addClass('vhs-icon-pause').removeClass('vhs-icon-play')
+  }
+
+  onPause() {
+    this.ui.play.addClass('vhs-icon-play').removeClass('vhs-icon-pause')
+  }
+
+  render() {
+    this.style = Styler.getStyleFor(controlsStyle)
+    this.template = template(controlsHTML)
+    this.$el.html(this.template())
+
+    this.core.$el.append(this.$el[0])
+    this.core.$el.append(this.style[0])
+
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.bind)
     return this
   }
 }
