318b85015271cd2cb6e0e2c08afb883e4a52ecb1
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-05-15 21:39:30 -0400

    update controls to inherit from media control

--- src/nyt_controls/index.js
+++ src/nyt_controls/index.js
@@ -1,86 +1,70 @@
-import {UICorePlugin, Utils, Events, Styler, template} from 'Clappr'
+import {$, UICorePlugin, Utils, MediaControl, Events, Styler, template} from 'Clappr'
 import controlsHTML from './public/controls.html'
 import controlsStyle from './public/controls.scss'
 
-export default class NYTControls extends UICorePlugin {
-  get name() { return 'NYTControls' }
-
+export default class NYTControls extends MediaControl {
   get events() {
     return {
-      'click .controls-play': 'togglePlay',
-      'mousedown .controls-progress-timeline': 'seekDrag',
-      'mousemove .controls-progress-timeline': 'seekDrag',
-      'mouseleave .controls-progress-timeline': 'finishSeekDrag'
+      'click .controls-play': 'togglePlayPause',
+      //      'click [data-stop]': 'stop',
+      //      'click [data-playstop]': 'togglePlayStop',
+      //      'click [data-fullscreen]': 'toggleFullscreen',
+      //      'click .bar-container[data-seekbar]': 'seek',
+      //      'click .bar-container[data-volume]': 'onVolumeClick',
+      //      'click .drawer-icon[data-volume]': 'toggleMute',
+      //      'click [data-cc-button]': 'toggleClosedCaptions',
+      //      'mouseenter .drawer-container[data-volume]': 'showVolumeBar',
+      //      'mouseleave .drawer-container[data-volume]': 'hideVolumeBar',
+      //      'mousedown .bar-container[data-volume]': 'startVolumeDrag',
+      //      'mousemove .bar-container[data-volume]': 'mousemoveOnVolumeBar',
+      //      'mousedown .bar-scrubber[data-seekbar]': 'startSeekDrag',
+      //      'mousemove .bar-container[data-seekbar]': 'mousemoveOnSeekBar',
+      //      'mouseleave .bar-container[data-seekbar]': 'mouseleaveOnSeekBar',
+      //      'mouseenter .media-control-layer[data-controls]': 'setUserKeepVisible',
+      //      'mouseleave .media-control-layer[data-controls]': 'resetUserKeepVisible'
     }
+
   }
 
-  togglePlay() { 
-    if (this.core.mediaControl.container.isPlaying()) {
-      this.core.mediaControl.pause()
+  changeTogglePlay() {
+    if (this.container && this.container.isPlaying()) {
+      this.$playPauseToggle.addClass('vhs-icon-pause').removeClass('vhs-icon-play')
     } else {
-      this.core.mediaControl.play()
+      this.$playPauseToggle.addClass('vhs-icon-play').removeClass('vhs-icon-pause')
     }
   }
 
-  seekDrag(e) {
-    console.log("seek drag", e)
+  createCachedElements() {
+    const $layer = this.$el.find('.controls')
+    console.log("creating cached elements", $layer.length)
+    this.$duration = $layer.find('.controls-duration')
+    this.$fullscreenToggle = $layer.find('.controls-fullscreen')
+    this.$playPauseToggle = $layer.find('.controls-play')
+    this.$playStopToggle = $layer.find('.controls-play')
+    this.$position = $layer.find('.controls-current-time')
+    this.$seekBarContainer = $layer.find('.controls-progress-timeline')
+    this.$seekBarHover = $layer.find('.controls-progress-buffer-and-time')
+    this.$seekBarLoaded = $layer.find('.controls-progress-buffer')
+    this.$seekBarPosition = $layer.find('.controls-progress-marker')
+    this.$seekBarScrubber = $layer.find('.controls-progress-scrubber')
+    this.$volumeBarContainer = $layer.find('.controls-volume-slider-container')
+    this.$volumeContainer = $layer.find('.controls-volume')
+    this.$volumeIcon = $layer.find('.controls-volume')
+    this.$volumeBarBackground = this.$el.find('.controls-volume-slider-track')
+    this.$volumeBarFill = this.$el.find('.controls-volume-slider-fill')
+    this.$volumeBarScrubber = this.$el.find('.controls-volume-slider')
+    this.$hdIndicator = this.$el.find('.controls-hd')
   }
 
-  finishSeekDrag(e) {
-    console.log("finish seek drag", e)
-  }
-
-  bind() {
-    this.ui = {
-      play: this.$el.find('.controls-play'),
-      progress: this.$el.find('.controls-progress'),
-      progressTimeline: this.$el.find('.controls-progress-timeline'),
-      progressBuffer: this.$el.find('.controls-progress-buffer'),
-      progressTime: this.$el.find('.controls-progress-time'),
-      marker: this.$el.find('.controls-progress-marker'),
-      duration: this.$el.find('.controls-duration'),
-      currentTime: this.$el.find('.controls-current-time'),
-    };
-
-    let container = this.core.mediaControl.container
-    this.listenTo(container, Events.CONTAINER_PLAY, this.onPlay)
-    this.listenTo(container, Events.CONTAINER_PAUSE, this.onPause)
-    this.listenTo(container, Events.CONTAINER_TIMEUPDATE, this.onTimeUpdate)
-    this.listenTo(container, Events.CONTAINER_SEEK, this.onSeek)
-    this.listenTo(container, Events.CONTAINER_ERROR, this.onError)
-  }
-
-  onPlay() {
-    this.ui.play.addClass('vhs-icon-pause').removeClass('vhs-icon-play')
-  }
-
-  onPause() {
-    this.ui.play.addClass('vhs-icon-play').removeClass('vhs-icon-pause')
-  }
-
-  onTimeUpdate(e) {
-    this.ui.duration.text(Utils.formatTime(e.total))
-    this.ui.currentTime.text(Utils.formatTime(e.current))
-
-    const percent = this.getSeekPosition(e);
-    this.ui.progressTime.css({ width: `${percent}%` });
-    this.ui.marker.css({ left: `${percent}%` });
-  }
-
-  getSeekPosition(e) {
-    const position = (e.current / e.total) * 100;
-    const trimmedPosition = Math.max(0, Math.min(position, 100));
-    return parseFloat(trimmedPosition.toFixed(2));
-  }
+  // should change the icon if Fullscreen.isFullscreen()
+  playerResize() {}
 
   render() {
+    super.render()
     this.style = Styler.getStyleFor(controlsStyle)
     this.$el.html(template(controlsHTML)())
-
-    this.core.$el.append(this.$el[0])
-    this.core.$el.append(this.style[0])
-
-    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.bind)
+    this.$el.append(this.style[0])
+    this.createCachedElements()
     return this
   }
 }
